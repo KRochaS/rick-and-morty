204a5af9cfe3ce5676f0c3625db8eb58238f3cd4
KRochaS <karinerocha.s@hotmail.com>
2024-01-05 11:05:43 -0300

    feat: Configs

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,23 @@
 import type { Config } from 'tailwindcss'
 
 const config: Config = {
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+    content: [
+        './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+        './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+        './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    ],
+    theme: {
+        extend: {
+            backgroundImage: {
+                'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+                'gradient-conic':
+                    'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+            },
+            boxShadow: {
+                lg: '0px 0.77px 2.717px 0px rgba(0, 0, 0, 0.02), 0px 2.129px 7.513px 0px rgba(0, 0, 0, 0.03), 0px 5.125px 18.088px 0px rgba(0, 0, 0, 0.04), 0px 17px 60px 0px rgba(0, 0, 0, 0.06)'
+            }
+        },
     },
-  },
-  plugins: [],
+    plugins: [],
 }
 export default config
