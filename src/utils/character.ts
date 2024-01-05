35976385597f65928d9d608d887914c56fa04650
KRochaS <karinerocha.s@hotmail.com>
2024-01-05 11:10:55 -0300

    feat: Utils utilizado para formatar os dados vindo da api

---
+++ src/utils/character.ts
@@ -0,0 +1,13 @@
+import { CharacterSpecie, CharacterStatus } from "@/models/character";
+
+export const characterStatus: CharacterStatus = {
+    Alive: 'Vivo',
+    Dead: 'Morto',
+    unknown: 'Desconhecido'
+};
+
+export const characterSpecie: CharacterSpecie = {
+    Human: 'Humano',
+    Alien: 'Alienígena',
+
+};
